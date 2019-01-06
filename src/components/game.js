b5ce65eb9ae62a71518218a27437ce45556fca05
Craig Pounds <nweaver@windstream.net>
2019-01-06 05:08:21 -0600

    Added about page

--- src/components/game.js
+++ src/components/game.js
@@ -7,47 +7,59 @@ import FormHotCold from './form-hot-cold';
 import Score from './score';
 
 export default class Game extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      comment: '',
+      guesses: [],
+      displayAbout: false
+    };
+  }
 
-  aboutPage() {
-    alert('what');
+  setAbout(about) {
+    this.setState({
+      displayAbout: about
+    });
   }
 
   newGame() {
     alert('new game');
   }
 
+  render() {
+    if (this.state.displayAbout) {
+      return <WhatAbout gotIt={() => this.setState({displayAbout: false})}/>
+    } else {
+      return (
+          <div className="game">
+            <Header 
+              onWhat={() => this.setAbout(true)}
+              onNewGame={() => this.newGame()}
+            />
+            <main className="main">
+              <Comment />
+              <FormHotCold />
+              <Score />
+            </main>
+          </div>   
+      );
+    }
+    
+  };
+
   // render() {
-  //   return (
-  //     <div className="page">
-  //     <WhatAbout />
-  //       <div className="game">
-  //         <Header 
-  //           onWhat={() => this.aboutPage()}
-  //           onNewGame={() => this.newGame()}
-  //         />
-  //         <main className="main">
-  //           <Comment />
-  //           <FormHotCold />
-  //           <Score />
-  //         </main>
-  //       </div>        
-  //     </div>
+  //   return (     
+  //     <div className="game">
+  //       <Header 
+  //         onWhat={() => this.displayAbout()}
+  //         onNewGame={() => this.newGame()}
+  //       />
+  //       <main className="main">
+  //         <Comment />
+  //         <FormHotCold />
+  //         <Score />
+  //       </main>
+  //     </div>  
   //   );
   // };
-
-  render() {
-    return (     
-      <div className="game">
-        <Header 
-          onWhat={() => this.aboutPage()}
-          onNewGame={() => this.newGame()}
-        />
-        <main className="main">
-          <Comment />
-          <FormHotCold />
-          <Score />
-        </main>
-      </div>  
-    );
-  };
 };
--- src/components/header.js
+++ src/components/header.js
@@ -2,9 +2,7 @@ import React from 'react';
 import './header.css';
 
 export default function Header(props) {
-
-  
-
+ 
   return (
     <header className="header" role="banner">
       <nav className="navigation">
