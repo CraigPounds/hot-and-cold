83799b7f84a76bd57527281aadcc193f14779a23
Craig Pounds <nweaver@windstream.net>
2019-01-06 05:50:42 -0600

    Cleaning up styles

--- src/components/what-about.js
+++ src/components/what-about.js
@@ -9,9 +9,9 @@ export default function WhatAbout(props) {
         <h3>What do I do?</h3>
         <p>This is a Hot or Cold Number Guessing Game. The game goes like this:</p>
         <ol className="instructions">
-          <li>I pick a <span className="underlined">random secret number</span> between 1 to 100 and keep it hidden.</li>
-          <li>You need to <span className="underlined">guess</span> until you can find the hidden secret number.</li>
-          <li>You will <span className="underlined">get feedback</span> on how close 	&#40;&quot;hot&quot;&#41; or far 	&#40;&quot;cold&quot;&#41; your guess is.</li>
+          <li>1. I pick a <span className="underlined">random secret number</span> between 1 to 100 and keep it hidden.</li>
+          <li>2. You need to <span className="underlined">guess</span> until you can find the hidden secret number.</li>
+          <li>3. You will <span className="underlined">get feedback</span> on how close 	&#40;&quot;hot&quot;&#41; or far 	&#40;&quot;cold&quot;&#41; your guess is.</li>
         </ol>
         <p>So, Are you ready?</p>
         <button className="got-it-button" onClick={() => props.gotIt()}>Got It!</button>
